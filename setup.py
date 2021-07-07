037842a746a4c869d23abb7e994b344772538190
Hunter Fernandes <hgfern@gmail.com>
2021-07-06 22:51:12 -0700

    Add multidevice support. Use the --device flag.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="winix",
-    version="0.2.1",
+    version="0.3.0",
     author="Hunter Fernandes",
     author_email="hunter@hfernandes.com",
     description="Programmatically control the Winix C545",
